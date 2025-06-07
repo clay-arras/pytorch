2204a9124ac55210d3dfc6259d18b26c7da4aea0
Neil Lin <astrollin.neil@gmail.com>
2025-06-06 22:52:47 -0400

    Update all_test.py

--- benchmarks/operator_benchmark/pt/all_test.py
+++ benchmarks/operator_benchmark/pt/all_test.py
@@ -11,7 +11,6 @@ cross_product_config = {
 }
 
 
-
 binary_ops_list = op_bench.op_list(
     attr_names=["op_name", "op_func"],
     attrs=[
@@ -29,7 +28,7 @@ binary_configs_broadcast = op_bench.config_list(
     ],
     cross_product_configs=cross_product_config,
 )
-binary_configs_broadcast = op_bench.config_list(
+binary_configs_same_size = op_bench.config_list(
     attr_names=["in_one", "in_two"],
     attrs=[
         [[128, 128], [128, 128]],
@@ -44,8 +43,8 @@ binary_configs_broadcast = op_bench.config_list(
 class BinaryOpBenchmark(op_bench.TorchBenchmarkBase):
     def init(self, in_one, in_two, dtype, device, op_func):
         self.inputs = {
-            "in_one": torch.randn(in_one, device=device).to(dtype=dtype),
-            "in_two": torch.randn(in_two, device=device).to(dtype=dtype),
+            "in_one": torch.randn(in_one, device=device, requires_grad=True).to(dtype=dtype),
+            "in_two": torch.randn(in_two, device=device, requires_grad=True).to(dtype=dtype),
         }
         self.op_func = op_func
 
@@ -54,16 +53,22 @@ class BinaryOpBenchmark(op_bench.TorchBenchmarkBase):
 
 
 op_bench.generate_pt_tests_from_op_list(
-    binary_ops_list, binary_configs_broadcast, BinaryOpBenchmark
+    binary_ops_list, binary_configs_same_size, BinaryOpBenchmark
+)
+op_bench.generate_pt_gradient_tests_from_op_list(
+    binary_ops_list, binary_configs_same_size, BinaryOpBenchmark
 )
 op_bench.generate_pt_tests_from_op_list(
-    binary_ops_list, binary_configs, BinaryOpBenchmark
+    binary_ops_list, binary_configs_broadcast, BinaryOpBenchmark
+)
+op_bench.generate_pt_gradient_tests_from_op_list(
+    binary_ops_list, binary_configs_broadcast, BinaryOpBenchmark
 )
 
 
 # ---
 
-unary_ops_configs_short = op_bench.config_list(
+unary_ops_configs = op_bench.config_list(
     attr_names=["in_one"],
     attrs=[
         [[128, 128]],
@@ -76,7 +81,7 @@ unary_ops_configs_short = op_bench.config_list(
 
 class UnaryOpBenchmark(op_bench.TorchBenchmarkBase):
     def init(self, in_one, device, op_func):
-        self.inputs = {"input": torch.rand(in_one, device=device)}
+        self.inputs = {"input": torch.rand(in_one, device=device, requires_grad=True)}
         self.op_func = op_func
 
     def forward(self, input):
@@ -102,9 +107,48 @@ unary_ops_list = op_bench.op_list(
 
 
 op_bench.generate_pt_tests_from_op_list(
-    unary_ops_list, unary_ops_configs_short + unary_ops_configs_long, UnaryOpBenchmark
+    unary_ops_list, unary_ops_configs, UnaryOpBenchmark
+)
+op_bench.generate_pt_gradient_tests_from_op_list(
+    unary_ops_list, unary_ops_configs, UnaryOpBenchmark
 )
 
+
+
+reduction_configs = op_bench.cross_product_configs(
+    R=[64, 256], 
+    V=[32, 512], 
+    dim=[0, 1],
+    device=["cpu", "cuda"],
+) 
+
+class ReductionBenchmark(op_bench.TorchBenchmarkBase):
+    def init(self, R, V, dim, device, op_func, op_name):
+        shape = (R, V) if dim == 0 else (V, R)
+        self.input_tensor = torch.rand(shape, device=device, requires_grad=True)
+        self.inputs = {"input_tensor": self.input_tensor, "dim": dim}
+        self.op_func = op_func
+        self.set_module_name(op_name)
+
+    def forward(self, input_tensor, dim: int):
+        return self.op_func(input_tensor, dim)
+
+reduction_ops_list = op_bench.op_list(
+    attr_names=["op_name", "op_func"],
+    attrs=[
+        ["sum", lambda x, dim: x.sum(dim=dim)],
+        ["min", lambda x, dim: x.min(dim=dim).values],
+        ["max", lambda x, dim: x.max(dim=dim).values], 
+        ["prod", lambda x, dim: x.prod(dim=dim)],
+    ],
+)
+
+op_bench.generate_pt_tests_from_op_list(
+    reduction_ops_list, reduction_configs, ReductionBenchmark
+)
+op_bench.generate_pt_gradient_tests_from_op_list(
+    reduction_ops_list, reduction_configs, ReductionBenchmark
+)
 
 
 
